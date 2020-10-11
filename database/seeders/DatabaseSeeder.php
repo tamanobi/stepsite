d0bc411636f407da2b4e69bfb13067b649a984e7
tamanobi <tamanobi@gmail.com>
2020-10-12 00:23:07 +0900

    Groupという概念を作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        $groups = ['無料会員', 'シルバー会員', 'ゴールド会員'];
+        foreach ($groups as $group) {
+            \App\Models\Group::create(['name' => $group]);
+        }
     }
 }
